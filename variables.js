81f5d3db5d54a9840ebecded58af776a252000aa
nayon117 <hasibul.nayon1@gmail.com>
2024-01-04 10:51:05 +0600

    learn about variables with examples

---
+++ variables.js
@@ -0,0 +1,5 @@
+// variable is a container which can store data
+// examples 
+const name = "Hasibul Hasan Nayon"
+const age = 23;
+console.log("my name is " + name, "and i am "+age + " years old");
