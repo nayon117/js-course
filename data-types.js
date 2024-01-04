8e952e1a14c218f66a587e14dff88792d76d9e2c
nayon117 <hasibul.nayon1@gmail.com>
2024-01-04 11:23:51 +0600

    learn about numbers

--- data-types.js
+++ data-types.js
@@ -5,3 +5,26 @@
 
 // 2.Non-primitive-non-primitive are user defined data types
 //  Array, Object, Function
+
+// -----------------Number ---------------
+// we can  get 64 bit to store a floating number
+// we can  get 54 bit to store a Integer number
+
+// Integer 
+const n = 1555;
+// Floating
+const m = 14.67;
+
+// we can convert a string into a number by using Number;
+// ex:
+const l = "45"
+// console.log(Number(l));
+
+// we can convert a number into a floating number by using parseFloat
+// we can convert a Floating number into a integer by using parseInt
+
+// console.log(Number.parseFloat(l));
+// console.log(Number.parseInt(l));
+
+// console.log(Number.MAX_VALUE);  to see the higest value we can store
+// console.log(Number.MIN_SAFE_INTEGER); to see the lowest value we can store;
