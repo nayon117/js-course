6676906b8869d2f34fffb2804d2e4fba929d1728
nayon117 <hasibul.nayon1@gmail.com>
2024-01-04 11:02:50 +0600

    learn about JS data types

---
+++ data-types.js
@@ -0,0 +1,7 @@
+// There are two types of data types in javascript
+
+// 1.primitive - primitive types are defined by JS
+// number, string, boolean, null undefined
+
+// 2.Non-primitive-non-primitive are user defined data types
+//  Array, Object, Function
