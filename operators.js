60fecd137e594c7c2bee39eec85362b93100699b
nayon117 <hasibul.nayon1@gmail.com>
2024-01-05 10:26:41 +0600

    learn about operators

---
+++ operators.js
@@ -0,0 +1,51 @@
+// Arithmetic operator  +, -, *, /, %, ++, --
+
+// there are two types of incremental pre and post incremental and also same thing for decremental.
+
+// if ++ before then pre else post incremental . post incremental print out fast then increment whereas pre incremental increment before then printout
+
+let a = 11;
+let b = 6;
+// console.log(a++);
+// console.log(++a);
+// console.log(a);
+
+let c = 10;
+let d = 20;
+c += d; 
+// console.log(c);
+c -= d;
+// console.log(c);
+c *= d;
+// console.log(c);
+c /= d;
+// console.log(c);
+c %= d;
+// console.log(c);
+
+// ----------comparison---------------------------------
+let e = 10;
+let f = 20;
+// console.log(e==f);
+// console.log(e!=f);
+// console.log(e>f);
+// console.log(e<f);
+e = 20;
+// console.log(e>=f);
+// console.log(e<=f);
+
+const i = '50'
+const j = 50;
+console.log(i==j);
+console.log(i === j);
+
+// ------logical operator-----------
+// && - all condition must be true;
+// || - return true if one condition true;
+// ! - Not oprator used for reverse
+
+// Bitwise operator
+// &, |, ~, ^, <<, >>
+
+// type of operator used to check the data type
+console.log(typeof(10));
