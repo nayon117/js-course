ce42b6ad42f11d41436da71fd85612525e7067dc
nayon117 <hasibul.nayon1@gmail.com>
2024-01-06 10:57:29 +0600

    learn about math functions

---
+++ math.js
@@ -0,0 +1,18 @@
+// Function are core building blocks of any programming language.
+
+// constants
+// console.log(Math.E);
+// console.log(Math.PI);
+let n = 4.568;
+// console.log(Math.abs(n));
+// console.log(Math.floor(n));
+// console.log(Math.ceil(n));
+// console.log(Math.round(n));
+// console.log(Math.max(200,300,400));
+// console.log(Math.min(200,300,400));
+// console.log(Math.pow(2,3));
+// console.log(Math.sqrt(9));
+
+// random number generator
+// console.log(Math.random());
+console.log(Math.round(Math.random() * 50 + 1));
