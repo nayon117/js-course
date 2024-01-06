8dfc022b03e90319ad3e4be9279d0de0f837ff56
nayon117 <hasibul.nayon1@gmail.com>
2024-01-06 10:07:23 +0600

    learn about statements in js

---
+++ statement.js
@@ -0,0 +1,8 @@
+// statements are instructions that are executed by the browser . A statement can be simple or complex operation that performs an action.
+
+// Example
+console.log('Hello world');
+const str = 'string'
+const a = 10;
+const b = 20;
+// all are statements 
