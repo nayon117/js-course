faf80f3defeb276b05745570d2bc3792b6be8222
nayon117 <hasibul.nayon1@gmail.com>
2024-01-07 17:15:46 +0600

    learn about else if condition

--- logic.js
+++ logic.js
@@ -5,19 +5,22 @@
 // switch statements
 
 // if---------------------------
-const a = 10;
+const a = 20;
 const b = 20;
 if (a > b) {
   console.log("A is greater than B");
-}
-else {
+} else if(a<b) {
   console.log("B is greater than A");
+} else {
+    console.log('They both are same');
 }
 
-const n = 5;
-if (n % 2 === 0) {
-  console.log(n + " is Even number");
+const n = 0;
+if (n === 0) {
+    console.log(n+" is zero");
 }
-else {
+else if (n % 2 === 0) {
+  console.log(n + " is Even number");
+} else {
   console.log(n + " is odd number");
 }
