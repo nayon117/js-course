61c295bc5663c9e9a23d94b6dbafb92072cda561
nayon117 <hasibul.nayon1@gmail.com>
2024-01-07 17:32:20 +0600

    learn about switch statement

---
+++ switch.js
@@ -0,0 +1,27 @@
+const date = new Date()
+const today = date.getDay()
+
+switch (today) {
+    case 0:
+        console.log("Today is sunday");
+        break;
+    case 1:
+        console.log("Today is Monday");
+        break;
+    case 2:
+        console.log("Today is Tuesday");
+        break;
+    case 3:
+        console.log("Today is Wednesday");
+        break;
+    case 4:
+        console.log("Today is Thrusday");
+        break;
+    case 5:
+        console.log("Today is Friday");
+        break;
+    case 6:
+        console.log("Today is Satarday");
+        break;
+    default: console.log("Not a day");
+}
