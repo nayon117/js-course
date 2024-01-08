6d87577bc6803789b2679ba9461b5afd9009a74a
nayon117 <hasibul.nayon1@gmail.com>
2024-01-08 07:49:13 +0600

    know basics about loops

---
+++ loops.js
@@ -0,0 +1,4 @@
+// Loops are statements which execute some blocks of code repeatedly until the condition becomes false
+
+// There are mainly three types of loops 
+// for loop, while loop, do-while loops 
