5b633a34ba34198a56d94c1c3978bdd23e1154d6
nayon117 <hasibul.nayon1@gmail.com>
2024-01-09 18:32:53 +0600

    print odd number using for loop

--- for-loop.js
+++ for-loop.js
@@ -1,4 +1,11 @@
 // for loop 
 for (let i = 0; i < 100; i++){
-    console.log(i);
+    // console.log(i);
+}
+
+// print odd number
+for (let i = 1; i <= 100; i++){
+    if (i % 2 === 1) {
+        console.log(i);
+    }
 }
