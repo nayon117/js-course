87c62e566f49caa731e0a892304159fc7f24f853
nayon117 <hasibul.nayon1@gmail.com>
2024-01-09 18:35:32 +0600

    implement addition using for loop

--- for-loop.js
+++ for-loop.js
@@ -13,6 +13,12 @@ for (let i = 1; i <= 100; i++){
 // print even number
 for (let i = 1; i <= 100; i++){
     if (i % 2 === 0) {
-        console.log(i);
+        // console.log(i);
     }
+}
+// addition
+let sum = 0;
+for (let i = 1; i <= 10; i++){
+    sum += i 
+    console.log(sum);
 }
