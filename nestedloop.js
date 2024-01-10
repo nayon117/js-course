14b783983ae043c878fb2512969619493c18b691
nayon117 <hasibul.nayon1@gmail.com>
2024-01-10 15:52:56 +0600

    print star using while loop

--- nestedloop.js
+++ nestedloop.js
@@ -1,6 +1,17 @@
-let n = 5;
+// let n = 5;
+// for (let i = 1; i <= n; i++){
+//     let result = ''
+//     for (j = 1; j <= i; j++){
+//         result += j + ' '
+//     }
+//     // console.log(result);
+// }
+
+let n = 10;
 for (let i = 1; i <= n; i++){
-    for (j = 1; j <= i; j++){
-        console.log(j);
+    let result = ''
+    for (let j = 1; j <= n; j++){
+        result += '* '
     }
+    console.log(result);
 }
