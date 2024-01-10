c09a5af49f6cda27467886c15e7a2531d872010b
nayon117 <hasibul.nayon1@gmail.com>
2024-01-10 15:22:42 +0600

    create winner game using while loop

--- while.js
+++ while.js
@@ -1,5 +1,16 @@
 let i = 0;
 while (i < 10) {
-    console.log('Hasibul Hasan Nayon');
+    // console.log('Hasibul Hasan Nayon');
     i++;
+}
+
+let isRunning = true;
+while (isRunning) {
+    let rand = Math.floor(Math.random() * 10 + 1)
+    if (rand === 9) {
+        console.log('winner winner winner');
+        isRunning = false;
+    } else {
+        console.log('you have got', + rand);
+    }
 }
