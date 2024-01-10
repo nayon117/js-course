9a5b93c37bc5de6643952d6811ca9ed51ebdb4da
nayon117 <hasibul.nayon1@gmail.com>
2024-01-10 16:05:48 +0600

    create example for break statement

--- break.js
+++ break.js
@@ -7,3 +7,11 @@ while (true) {
         console.log('you have got', + rand);
     }
 }
+
+for (let i = 1; i <= 10; i++){
+    if (i % 5 === 0) {
+        break;
+    } else {
+        console.log(i);
+    }
+}
