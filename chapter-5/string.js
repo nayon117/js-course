798695bf3370de2b12b3b7751590148e22f35a1a
nayon117 <hasibul.nayon1@gmail.com>
2024-01-14 19:16:03 +0600

    learn more about string

---
+++ chapter-5/string.js
@@ -0,0 +1,17 @@
+// string literal
+const str = 'string'
+
+// string constructor
+const str2 = String('string')
+console.log(str2);
+
+// why use constructor 
+// const n = 10;
+// // use empty string to convert number to string
+// const str3 = n + ''
+// console.log(str3);
+
+const n = 10;
+// use constructor to convert number to string
+const str3 = String(n)
+console.log(str3);
