54a7fd90187330639c12a577f28f232fc1394735
nayon117 <hasibul.nayon1@gmail.com>
2024-01-25 20:55:07 +0600

    add break example

--- break.js
+++ break.js
@@ -15,3 +15,10 @@ for (let i = 1; i <= 10; i++){
         console.log(i);
     }
 }
+for (let i = 1; i <= 20; i++){
+    if (i % 5 === 0) {
+        break;
+    } else {
+        console.log(i);
+    }
+}
