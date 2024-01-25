795b06fd400ea4af6443c474a0a63aeff579e01d
nayon117 <hasibul.nayon1@gmail.com>
2024-01-25 20:56:08 +0600

    add another continue example

--- continue.js
+++ continue.js
@@ -7,3 +7,10 @@ for (let i = 0; i <= 10; i++){
         console.log(i);
     }
 }
+for (let i = 0; i <= 20; i++){
+    if (i === 3 || i === 7) {
+        continue;
+    } else {
+        console.log(i);
+    }
+}
