22a5048b2521875c303b96de3578e46bc5b4121a
nayon117 <hasibul.nayon1@gmail.com>
2024-02-15 21:22:21 +0600

    learn more about switch

--- switch.js
+++ switch.js
@@ -25,3 +25,30 @@ switch (today) {
         break;
     default: console.log("Not a day");
 }
+const date1 = new Date()
+const thisDay = date.getDay()
+
+switch (thisDay) {
+    case 0:
+        console.log("Today is sunday");
+        break;
+    case 1:
+        console.log("Today is Monday");
+        break;
+    case 2:
+        console.log("Today is Tuesday");
+        break;
+    case 3:
+        console.log("Today is Wednesday");
+        break;
+    case 4:
+        console.log("Today is Thrusday");
+        break;
+    case 5:
+        console.log("Today is Friday");
+        break;
+    case 6:
+        console.log("Today is Satarday");
+        break;
+    default: console.log("Not a day");
+}
